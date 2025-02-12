e6c4477d1fc5e389b42ac34804328b50b0e63fed
Benson-Gikonyo <muhiugikonyo@gmail.com>
2025-02-12 17:28:53 +0300

    bug fix

--- api/routes/books.py
+++ api/routes/books.py
@@ -50,10 +50,11 @@ async def get_books() -> OrderedDict[int, Book]:
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id:int) -> Book:
     book = db.get_book(book_id)
-    if not book:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    if book:
+        return book
+    
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
 
-    return book
     
     
 
